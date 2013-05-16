efa35ea784403f7e393c07377751af86e822705d
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2013-05-16 09:33:05 +0300

    Initial Commit

---
+++ Planets/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Planets
+//
+//  Created by Mihai Iachimovschi on 16.05.2013.
+//  Copyright (c) 2013 Mihai Iachimovschi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
